a2cef0e1f47c9397cf36a768c41116bd6c346e9b
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-06-06 00:02:04 +0300

    1

---
+++ main.js
@@ -0,0 +1,43 @@
+const { program } = require('commander');
+const TaskManager = require('./taskManager');
+
+const taskManager = new TaskManager();
+
+program.version('1.0.0').description('Task Tracker');
+
+program
+  .command('list')
+  .description('Show all tasks')
+  .action(() => {
+    taskManager.listTasks();
+  });
+
+program
+  .command('add <title> [description] [deadline]')
+  .description('Add a new task')
+  .action((title, description, deadline) => {
+    taskManager.addTask(title, description, deadline);
+  });
+
+program
+  .command('edit <taskId> [title] [description] [deadline]')
+  .description('Edit a task')
+  .action((taskId, title, description, deadline) => {
+    taskManager.editTask(taskId, title, description, deadline);
+  });
+
+program
+  .command('complete <taskId>')
+  .description('Mark a task as completed')
+  .action((taskId) => {
+    taskManager.completeTask(taskId);
+  });
+
+program
+  .command('delete <taskId>')
+  .description('Delete a task')
+  .action((taskId) => {
+    taskManager.deleteTask(taskId);
+  });
+
+program.parse(process.argv);
---
+++ taskManager.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+
+class TaskManager {
+  constructor() {
+    this.tasks = this.loadTasks();
+  }
+
+  loadTasks() {
+    try {
+      const data = fs.readFileSync('tasks.json');
+      return JSON.parse(data);
+    } catch (error) {
+      return [];
+    }
+  }
+
+  saveTasks() {
+    fs.writeFileSync('tasks.json', JSON.stringify(this.tasks));
+  }
+
+  listTasks() {
+    console.log('All tasks:');
+    this.tasks.forEach((task) => {
+      console.log(`- ${task.title} [${task.completed ? 'Completed' : 'Pending'}]`);
+      console.log(`  Description: ${task.description}`);
+      console.log(`  Deadline: ${task.deadline || 'No deadline'}`);
+      console.log(`  Completion Date: ${task.completionDate || 'Not completed'}`);
+      console.log('----------------------------');
+    });
+  }
+
+  addTask(title, description, deadline) {
+    const task = {
+      id: Date.now().toString(),
+      title,
+      description,
+      deadline,
+      completed: false,
+    };
+    this.tasks.push(task);
+    this.saveTasks();
+    console.log('Task added successfully.');
+  }
+
+  editTask(taskId, title, description, deadline) {
+    const task = this.findTaskById(taskId);
+    if (task) {
+      task.title = title || task.title;
+      task.description = description || task.description;
+      task.deadline = deadline || task.deadline;
+      this.saveTasks();
+      console.log('Task edited successfully.');
+    } else {
+      console.log('Task not found.');
+    }
+  }
+
+  completeTask(taskId) {
+    const task = this.findTaskById(taskId);
+    if (task) {
+      task.completed = true;
+      task.completionDate = new Date().toLocaleString();
+      this.saveTasks();
+      console.log('Task marked as completed.');
+    } else {
+      console.log('Task not found.');
+    }
+  }
+
+  deleteTask(taskId) {
+    const index = this.tasks.findIndex((task) => task.id === taskId);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+      this.saveTasks();
+      console.log('Task deleted successfully.');
+    } else {
+      console.log('Task not found.');
+    }
+  }
+
+  findTaskById(taskId) {
+    return this.tasks.find((task) => task.id === taskId);
+  }
+}
+
+module.exports = TaskManager;
