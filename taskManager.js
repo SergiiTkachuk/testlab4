7638648dfa6538ddc16164a5e4ffe7f13b7ef5ec
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-06-06 09:14:02 +0300

    final

--- taskManager.js
+++ taskManager.js
@@ -1,123 +1,172 @@
 const fs = require('fs');
 
 class TaskManager {
-  constructor(fileName) {
-   this.fileName = fileName
-   this.tasks = this.loadTasks(); 
-  }
-
-  loadTasks() {
-    try {
-      const data = fs.readFileSync(`${this.fileName}`);
-      return JSON.parse(data);
-    } catch (error) {
-      return [];
-    }
-  }
-
-  saveTasks() {
-    const jsonData = JSON.stringify(this.tasks, null, 2);
-    fs.writeFileSync(`${this.fileName}`, jsonData);
-  }
-  
-
-  listTasks() {
-    console.log('All tasks:');
-    this.tasks.forEach((task) => {
-      console.log(`  ID: ${task.id}`);
-      console.log(`  Title: ${task.title} [${task.completed ? 'Completed' : 'Pending'}]`);
-      console.log(`  Description: ${task.description || 'No description'}`);
-      console.log(`  Deadline: ${task.deadline || 'No deadline'}`);
-      console.log(`  Completion Date: ${task.completionDate ||'Not completed'}`);
-      console.log('----------------------------');
-    });
-  }
-
-  addTask(title, description, deadline) {
-    const task = {
-      id: Date.now().toString(),
-      title,
-      description,
-      deadline,
-      completed: false,
-    };
-    this.tasks.push(task);
-    this.saveTasks();
-    console.log('Task added successfully.');
-  }
-
-  editTask(taskId, title, description, deadline) {
-    const task = this.findTaskById(taskId);
-    if (task) {
-      task.title = title || task.title;
-      task.description = description || task.description;
-      task.deadline = deadline || task.deadline;
-      this.saveTasks();
-      console.log('Task edited successfully.');
-    } else {
-      console.log('Task not found.');
-    }
-  }
-
-  completeTask(taskId) {
-    const task = this.findTaskById(taskId);
-    if (task) {
-      task.completed = true;
-      task.completionDate = new Date().toLocaleString();
-      this.saveTasks();
-      console.log('Task marked as completed.');
-    } else {
-      console.log('Task not found.');
-    }
-  }
-
-  deleteTask(taskId) {
-    const index = this.tasks.findIndex((task) => task.id === taskId);
-    if (index !== -1) {
-      this.tasks.splice(index, 1);
-      this.saveTasks();
-      console.log('Task deleted successfully.');
-    } else {
-      console.log('Task not found.');
-    }
-  }
-
-  findTaskById(taskId) {
-    return this.tasks.find((task) => task.id === taskId);
-  }
-
-  showExpiredTasks() {
-   const currentDate = new Date();
-   const expiredTasks = this.tasks.filter((task) => !task.completed && task.deadline && new Date(task.deadline) < currentDate);
    
-   console.log('Expired tasks:');
-   expiredTasks.forEach((task) => {
-      console.log(`  ID: ${task.id}`);
-      console.log(`  Title: ${task.title}`);
-      console.log(`  Description: ${task.description}`);
-      console.log(`  Deadline: ${task.deadline}`);
-      console.log('----------------------------');
-   });
- }
+   constructor(fileName) {
+      
+      this.fileName = fileName;
+      this.tasks = this.loadTasks();
+   
+   }
+   
+   loadTasks() {
+      
+      try {
 
- showPendingTasks() {
-   const tasksWithDeadline = this.tasks.filter((task) => task.hasOwnProperty('deadline'));
-   const pendingTasks = tasksWithDeadline.filter((task) => !task.completed);
-   pendingTasks.sort((a, b) => {
-     if (!a.deadline) return -1;
-     if (!b.deadline) return 1;
-     return new Date(a.deadline) - new Date(b.deadline);
-   });
+         const data = fs.readFileSync(`${this.fileName}`);
+         return JSON.parse(data);
+      
+      } catch (error) {
+         
+         console.log(error);
+         return [];
+      }
+   }
+   
+   saveTasks() {
+      
+      const jsonData = JSON.stringify(this.tasks, null, 2);
+      fs.writeFileSync(`${this.fileName}`, jsonData);
+   
+   }
+  
+   listTasks() {
+      
+      console.log('All tasks:');
+      
+      this.tasks.forEach((task) => {
+      
+         console.log(`  ID: ${task.id}`);
+         console.log(`  Title: ${task.title} [${task.completed ? 'Completed' : 'Pending'}]`);
+         console.log(`  Description: ${task.description || 'No description'}`);
+         console.log(`  Deadline: ${task.deadline || 'No deadline'}`);
+         console.log(`  Completion Date: ${task.completionDate ||'Not completed'}`);
+         console.log('----------------------------');
 
-   console.log('Pending tasks (sorted by deadline):');
-   pendingTasks.forEach((task) => {
-     console.log(`  ID: ${task.Id}`);
-     console.log(`  Title: ${task.title}`);
-     console.log(`  Description: ${task.description || 'No description'}`);
-     console.log(`  Deadline: ${task.deadline}`);
-     console.log('----------------------------');
-   });
- }
+      });
+   }
+
+   addTask(title, description, deadline) {
+      
+      const task = {
+         
+         id: Date.now().toString(),
+         title,
+         description,
+         deadline,
+         completed: false,
+      
+      };
+      
+      this.tasks.push(task);
+      this.saveTasks();
+
+      console.log('Task added successfully.');
+   }
+   
+   editTask(taskId, title, description, deadline) {
+      
+      const task = this.findTaskById(taskId);
+      
+      if (task) {
+         
+         task.title = title || task.title;
+         task.description = description || task.description;
+         task.deadline = deadline || task.deadline;
+         
+         this.saveTasks();
+         
+         console.log('Task edited successfully.');
+      } else {
+         console.log('Task not found.');
+      }
+   }
+   
+   completeTask(taskId) {
+      
+      const task = this.findTaskById(taskId);
+      
+      if (task) {
+         
+         task.completed = true;
+         task.completionDate = new Date().toLocaleString();
+         
+         this.saveTasks();
+         
+         console.log('Task marked as completed.');
+      } else {
+         console.log('Task not found.');
+      }
+   }
+   
+   deleteTask(taskId) {
+      
+      const index = this.tasks.findIndex((task) => task.id === taskId);
+      
+      if (index !== -1) {
+         
+         this.tasks.splice(index, 1);
+         
+         this.saveTasks();
+         
+         console.log('Task deleted successfully.');
+      } else {
+         console.log('Task not found.');
+      }
+   }
+   
+   findTaskById(taskId) {
+      return this.tasks.find((task) => task.id === taskId);
+   }
+
+   showExpiredTasks() {
+      
+      const currentDate = new Date();
+      const expiredTasks = this.tasks.filter((task) => !task.completed && task.deadline && new Date(task.deadline) < currentDate);
+      
+      if (expiredTasks.length !== 0) {
+         
+         console.log('Expired tasks:');
+         
+         expiredTasks.forEach((task) => {
+            
+            console.log(`  ID: ${task.id}`);
+            console.log(`  Title: ${task.title}`);
+            console.log(`  Description: ${task.description}`);
+            console.log(`  Deadline: ${task.deadline}`);
+            console.log('----------------------------');
+         });
+      }
+   }
+   
+   showPendingTasks() {
+      
+      const tasksWithDeadline = this.tasks.filter((task) => task.hasOwnProperty('deadline'));
+      const pendingTasks = tasksWithDeadline.filter((task) => !task.completed);
+      
+      pendingTasks.sort((a, b) => {
+         return new Date(a.deadline) - new Date(b.deadline);
+      });
+      
+      console.log('Pending tasks (sorted by deadline):');
+      
+      if (pendingTasks.length === 0) {
+         
+         console.log('No pending tasks.');
+         return 1;
+      
+      }
+      
+      pendingTasks.forEach((task) => {
+         
+         console.log(`  ID: ${task.id}`);
+         console.log(`  Title: ${task.title}`);
+         console.log(`  Description: ${task.description || 'No description'}`);
+         console.log(`  Deadline: ${task.deadline}`);
+         console.log('----------------------------');
+      
+      });
+   }
 }
 
 module.exports = TaskManager;
--- tests.test.js
+++ tests.test.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 const TaskManager = require('./taskManager.js');
 
 describe('TaskManager', () => {
+  
   let taskManager;
 
   beforeEach(() => {
-    taskManager = new TaskManager();
+    taskManager = new TaskManager('test.json');
   });
 
   afterEach(() => {
@@ -13,8 +14,11 @@ describe('TaskManager', () => {
   });
 
   describe('loadTasks', () => {
+    
     test('should load tasks from test.json file', () => {
+      
       const tasks = [
+
         {
           id: '1',
           title: 'Task 1',
@@ -22,6 +26,7 @@ describe('TaskManager', () => {
           deadline: '2023-05-20',
           completed: false,
         },
+
         {
           id: '2',
           title: 'Task 2',
@@ -29,7 +34,9 @@ describe('TaskManager', () => {
           deadline: '2023-05-15',
           completed: true,
         },
+
       ];
+
       fs.writeFileSync('test.json', JSON.stringify(tasks));
 
       const loadedTasks = taskManager.loadTasks();
@@ -38,6 +45,7 @@ describe('TaskManager', () => {
     });
 
     test('should return an empty array if test.json file does not exist', () => {
+
       if (fs.existsSync('test.json')) {
         fs.unlinkSync('test.json');
       }
@@ -48,6 +56,7 @@ describe('TaskManager', () => {
     });
 
     test('should return an empty array if an error occurs while reading test.json file', () => {
+
       jest.spyOn(fs, 'readFileSync').mockImplementation(() => {
         throw new Error('Mocked readFileSync error');
       });
@@ -61,7 +70,9 @@ describe('TaskManager', () => {
   });
 
   describe('saveTasks', () => {
+
     test('should save tasks to test.json file', () => {
+
       taskManager.addTask('Task 1', 'Description 1', '2023-05-20');
 
       taskManager.saveTasks();
@@ -73,7 +84,9 @@ describe('TaskManager', () => {
   });
 
   describe('addTask', () => {
+
     test('should add a new task to the tasks list', () => {
+
       taskManager.addTask('Task 1', 'Description 1', '2023-05-20');
 
       expect(taskManager.tasks.length).toBe(1);
@@ -81,9 +94,11 @@ describe('TaskManager', () => {
       expect(taskManager.tasks[0].description).toBe('Description 1');
       expect(taskManager.tasks[0].deadline).toBe('2023-05-20');
       expect(taskManager.tasks[0].completed).toBe(false);
+
     });
 
     test('should generate a unique ID for each task', () => {
+
       taskManager.addTask('Task 1', 'Description 1', '2023-05-20');
       taskManager.addTask('Task 2', 'Description 2', '2023-05-15');
 
@@ -92,7 +107,9 @@ describe('TaskManager', () => {
   });
 
   describe('editTask', () => {
+
     test('should edit the specified task', () => {
+
       taskManager.addTask('Task 1', 'Description 1', '2023-05-20');
 
       taskManager.editTask(taskManager.tasks[0].id, 'New Title', 'New Description', '2023-05-25');
@@ -100,9 +117,11 @@ describe('TaskManager', () => {
       expect(taskManager.tasks[0].title).toBe('New Title');
       expect(taskManager.tasks[0].description).toBe('New Description');
       expect(taskManager.tasks[0].deadline).toBe('2023-05-25');
+
     });
 
     test('should not modify the task if no new values are provided', () => {
+
       taskManager.addTask('Task 1', 'Description 1', '2023-05-20');
 
       taskManager.editTask(taskManager.tasks[0].id);
@@ -110,11 +129,14 @@ describe('TaskManager', () => {
       expect(taskManager.tasks[0].title).toBe('Task 1');
       expect(taskManager.tasks[0].description).toBe('Description 1');
       expect(taskManager.tasks[0].deadline).toBe('2023-05-20');
+
     });
 
     test('should display a message if the task is not found', () => {
+
       const consoleLogMock = jest.fn();
       jest.spyOn(console, 'log').mockImplementation(consoleLogMock);
+
       taskManager.editTask('nonexistentId', 'New Title', 'New Description', '2023-05-25');
     
       expect(consoleLogMock).toHaveBeenCalledWith('Task not found.');
@@ -125,27 +147,35 @@ describe('TaskManager', () => {
   });
 
   describe('completeTask', () => {
+
     test('should mark the specified task as completed', () => {
+
       taskManager.addTask('Task 1', 'Description 1', '2023-05-20');
 
       taskManager.completeTask(taskManager.tasks[0].id);
 
       expect(taskManager.tasks[0].completed).toBe(true);
       expect(taskManager.tasks[0].completionDate).toBeDefined();
+
     });
 
     test('should display a message if the task is not found', () => {
+
       const consoleLogMock = jest.fn();
       jest.spyOn(console, 'log').mockImplementation(consoleLogMock);
+
       taskManager.completeTask('nonexistentId');
 
       expect(consoleLogMock).toHaveBeenCalledWith('Task not found.');
+
       console.log.mockRestore();
     });
   });
 
   describe('deleteTask', () => {
+
     test('should delete the specified task', () => {
+
       taskManager.addTask('Task 1', 'Description 1', '2023-05-20');
 
       taskManager.deleteTask(taskManager.tasks[0].id);
@@ -154,17 +184,22 @@ describe('TaskManager', () => {
     });
 
     test('should display a message if the task is not found', () => {
+
       const consoleLogMock = jest.fn();
       jest.spyOn(console, 'log').mockImplementation(consoleLogMock);
+
       taskManager.deleteTask('nonexistentId');
 
       expect(consoleLogMock).toHaveBeenCalledWith('Task not found.');
+
       console.log.mockRestore();
     });
   });
 
   describe('findTaskById', () => {
+
     test('should return the task with the specified ID', () => {
+
       taskManager.addTask('Task 1', 'Description 1', '2023-05-20');
 
       const foundTask = taskManager.findTaskById(taskManager.tasks[0].id);
@@ -173,6 +208,7 @@ describe('TaskManager', () => {
     });
 
     test('should return undefined if the task with the specified ID is not found', () => {
+      
       const foundTask = taskManager.findTaskById('nonexistentId');
 
       expect(foundTask).toBeUndefined();
@@ -180,81 +216,100 @@ describe('TaskManager', () => {
   });
 
   describe('showExpiredTasks', () => {
+
     test('should display a list of expired tasks', () => {
 
       taskManager.addTask('Task 1', 'Description 1', '2023-05-01');
       taskManager.addTask('Task 2', 'Description 2', '2023-04-30');
 
-      jest.spyOn(console, 'log');
+      const consoleLogMock = jest.fn();
+      jest.spyOn(console, 'log').mockImplementation(consoleLogMock);
 
       taskManager.showExpiredTasks();
 
-      expect(console.log).toHaveBeenCalledWith('Expired tasks:');
-      expect(console.log).toHaveBeenCalledWith(`  ID: ${taskManager[0].id}`);
-      expect(console.log).toHaveBeenCalledWith('  Title: Task 1');
-      expect(console.log).toHaveBeenCalledWith('  Description: Description 1');
-      expect(console.log).toHaveBeenCalledWith('  Deadline: 2023-05-01');
-      expect(console.log).toHaveBeenCalledWith('----------------------------');
-      expect(console.log).toHaveBeenCalledWith(`  ID: ${taskManager[1].id}`);
-      expect(console.log).toHaveBeenCalledWith('  Title: Task 2');
-      expect(console.log).toHaveBeenCalledWith('  Description: Description 2');
-      expect(console.log).toHaveBeenCalledWith('  Deadline: 2023-04-30');
-      expect(console.log).toHaveBeenCalledWith('----------------------------');
+      expect(consoleLogMock).toHaveBeenCalledWith('Expired tasks:');
+      expect(consoleLogMock).toHaveBeenCalledWith(`  ID: ${taskManager.tasks[0].id}`);
+      expect(consoleLogMock).toHaveBeenCalledWith('  Title: Task 1');
+      expect(consoleLogMock).toHaveBeenCalledWith('  Description: Description 1');
+      expect(consoleLogMock).toHaveBeenCalledWith('  Deadline: 2023-05-01');
+      expect(consoleLogMock).toHaveBeenCalledWith('----------------------------');
+      expect(consoleLogMock).toHaveBeenCalledWith(`  ID: ${taskManager.tasks[1].id}`);
+      expect(consoleLogMock).toHaveBeenCalledWith('  Title: Task 2');
+      expect(consoleLogMock).toHaveBeenCalledWith('  Description: Description 2');
+      expect(consoleLogMock).toHaveBeenCalledWith('  Deadline: 2023-04-30');
+      expect(consoleLogMock).toHaveBeenCalledWith('----------------------------');
 
       console.log.mockRestore();
     });
 
     test('should not display any tasks if no tasks are expired', () => {
+
       taskManager.addTask('Task 1', 'Description 1', '2023-05-20');
       taskManager.addTask('Task 2', 'Description 2', '2023-06-01');
 
-      jest.spyOn(console, 'log');
+      const firstTaskId = taskManager.tasks[0].id;
+      const secondTaskId = taskManager.tasks[1].id;
+
+      taskManager.completeTask(firstTaskId);
+      taskManager.completeTask(secondTaskId);
+
+      const consoleLogMock = jest.fn();
+      jest.spyOn(console, 'log').mockImplementation(consoleLogMock);
 
       taskManager.showExpiredTasks();
 
-      expect(console.log).not.toHaveBeenCalledWith('Expired tasks:');
+      expect(consoleLogMock).not.toHaveBeenCalledWith('Expired tasks:');
 
       console.log.mockRestore();
     });
   });
 
   describe('showPendingTasks', () => {
+
     test('should display a sorted list of pending tasks by deadline', () => {
+
       taskManager.addTask('Task 1', 'Description 1', '2023-05-20');
       taskManager.addTask('Task 2', 'Description 2', '2023-04-30');
-      taskManager.addTask('Task 3', 'Description 3', '2023-05-10');
+      taskManager.addTask('Task 3', '', '2023-05-10');
 
-      jest.spyOn(console, 'log');
+      const consoleLogMock = jest.fn();
+      jest.spyOn(console, 'log').mockImplementation(consoleLogMock);
 
       taskManager.showPendingTasks();
 
-      expect(console.log).toHaveBeenCalledWith('Pending tasks (sorted by deadline):');
-      expect(console.log).toHaveBeenCalledWith('- Task 2');
-      expect(console.log).toHaveBeenCalledWith('  Description: Description 2');
-      expect(console.log).toHaveBeenCalledWith('  Deadline: 2023-04-30');
-      expect(console.log).toHaveBeenCalledWith('----------------------------');
-      expect(console.log).toHaveBeenCalledWith('- Task 3');
-      expect(console.log).toHaveBeenCalledWith('  Description: Description 3');
-      expect(console.log).toHaveBeenCalledWith('  Deadline: 2023-05-10');
-      expect(console.log).toHaveBeenCalledWith('----------------------------');
-      expect(console.log).toHaveBeenCalledWith('- Task 1');
-      expect(console.log).toHaveBeenCalledWith('  Description: Description 1');
-      expect(console.log).toHaveBeenCalledWith('  Deadline: 2023-05-20');
-      expect(console.log).toHaveBeenCalledWith('----------------------------');
+      expect(consoleLogMock).toHaveBeenCalledWith('Pending tasks (sorted by deadline):');
+      expect(consoleLogMock).toHaveBeenCalledWith(`  ID: ${taskManager.tasks[1].id}`);
+      expect(consoleLogMock).toHaveBeenCalledWith('  Title: Task 2');
+      expect(consoleLogMock).toHaveBeenCalledWith('  Description: Description 2');
+      expect(consoleLogMock).toHaveBeenCalledWith('  Deadline: 2023-04-30');
+      expect(consoleLogMock).toHaveBeenCalledWith('----------------------------');
+      expect(consoleLogMock).toHaveBeenCalledWith(`  ID: ${taskManager.tasks[2].id}`);
+      expect(consoleLogMock).toHaveBeenCalledWith('  Title: Task 3');
+      expect(consoleLogMock).toHaveBeenCalledWith('  Description: No description');
+      expect(consoleLogMock).toHaveBeenCalledWith('  Deadline: 2023-05-10');
+      expect(consoleLogMock).toHaveBeenCalledWith('----------------------------');
+      expect(consoleLogMock).toHaveBeenCalledWith(`  ID: ${taskManager.tasks[0].id}`);
+      expect(consoleLogMock).toHaveBeenCalledWith('  Title: Task 1');
+      expect(consoleLogMock).toHaveBeenCalledWith('  Description: Description 1');
+      expect(consoleLogMock).toHaveBeenCalledWith('  Deadline: 2023-05-20');
+      expect(consoleLogMock).toHaveBeenCalledWith('----------------------------');
 
       console.log.mockRestore();
     });
 
     test('should display a message if there are no pending tasks', () => {
+
       taskManager.addTask('Task 1', 'Description 1', '2023-05-20');
+
       taskManager.completeTask(taskManager.tasks[0].id);
 
-      jest.spyOn(console, 'log');
+      const consoleLogMock = jest.fn();
+      jest.spyOn(console, 'log').mockImplementation(consoleLogMock);
 
       taskManager.showPendingTasks();
 
-      expect(console.log).toHaveBeenCalledWith('Pending tasks (sorted by deadline):');
-      expect(console.log).toHaveBeenCalledWith('No pending tasks.');
+      expect(consoleLogMock).toHaveBeenCalledWith('Pending tasks (sorted by deadline):');
+      expect(consoleLogMock).toHaveBeenCalledWith('No pending tasks.');
 
       console.log.mockRestore();
     });
